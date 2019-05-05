b6c942c5d0d769c9964bfa263e7516d2ff8b7142
xuhan <Han.Xu@paat.com>
2019-05-05 21:02:41 +0800

    单例设计模式

---
+++ java-basics/src/main/java/pers/xuhan/java/designpattern/singletonpattern/Singleton.java
@@ -0,0 +1,20 @@
+package pers.xuhan.java.designpattern.singletonpattern;
+
+/**
+ * @Filename: Singleton.java
+ * @Description: 单例模式: 创建简单可靠，缺点是JVM加载单例类的时候，单例对象就会被创建，没有实现延迟加载。
+ * @Version: 1.0
+ * @Author: han.xu
+ * @Email: han.xu@paat.com
+ * @date: 2019/5/5 20:09
+ */
+public class Singleton {
+
+    private Singleton(){
+        System.out.println("Singleton is create");//注意创建单例过程可能比较慢
+    }
+    private static Singleton instance = new Singleton();
+    public  static Singleton getInstance(){
+        return instance;
+    }
+}
