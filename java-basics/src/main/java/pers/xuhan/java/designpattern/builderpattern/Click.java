6ebde0cdd00b35aa6a46b325a41047f5fcf715f4
xuhan <Han.Xu@paat.com>
2019-05-21 16:02:51 +0800

    建造者模式

---
+++ java-basics/src/main/java/pers/xuhan/java/designpattern/builderpattern/Click.java
@@ -0,0 +1,15 @@
+package pers.xuhan.java.designpattern.builderpattern;
+
+public class Click {
+    public static void main(String[] args) {
+        // 非 Builder 模式 
+        Computer computer = new Computer("cpu", "screen", "memory", "mainboard"); 
+        // Builder 模式 
+        NewComputer newComputer = new NewComputer.Builder() 
+        .cpu("cpu") 
+        .screen("screen") 
+        .memory("memory") 
+        .mainboard("mainboard") 
+        .build();
+    } 
+} 
---
+++ java-basics/src/main/java/pers/xuhan/java/designpattern/builderpattern/Computer.java
@@ -0,0 +1,23 @@
+package pers.xuhan.java.designpattern.builderpattern;
+
+
+/**
+ * @Filename: Computer.java
+ * @Description: 电脑对象
+ * @Version: 1.0
+ * @Author: han.xu
+ * @Email: han.xu@paat.com
+ * @date: 2019/5/20 20:13
+ */
+public class Computer { 
+    private String cpu; 
+    private String screen; 
+    private String memory; 
+    private String mainboard; 
+    public Computer(String cpu, String screen, String memory, String mainboard) { 
+        this.cpu = cpu; 
+        this.screen = screen; 
+        this.memory = memory; 
+        this.mainboard = mainboard; 
+    }
+}
