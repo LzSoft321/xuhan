cb18917259bc673a449a5b39b9e0676f660c828c
xuhan <Han.Xu@paat.com>
2019-06-04 15:46:45 +0800

    注解

---
+++ java-basics/src/main/java/pers/xuhan/java/javabasic/注解/FruitName.java
@@ -0,0 +1,17 @@
+package pers.xuhan.java.javabasic.注解;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * 水果名称注解
+ */
+@Target(FIELD)
+@Retention(RUNTIME)
+@Documented
+public @interface FruitName {
+    String value() default "";
+}
---
+++ java-basics/src/main/java/pers/xuhan/java/javabasic/注解/FruitRun.java
@@ -0,0 +1,10 @@
+package pers.xuhan.java.javabasic.注解;
+
+/**
+ * 输出结果
+ */
+public class FruitRun {
+    public static void main(String[] args) {
+        FruitInfoUtil.getFruitInfo(Apple.class);
+    }
+}
