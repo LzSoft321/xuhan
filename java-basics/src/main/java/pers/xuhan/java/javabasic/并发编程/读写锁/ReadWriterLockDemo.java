c755346c41ceffffd9053b31656395701cf961ce
xuhan <Han.Xu@paat.com>
2019-06-13 13:52:54 +0800

    读写锁

---
+++ java-basics/src/main/java/pers/xuhan/java/javabasic/并发编程/读写锁/ReadWriterLockDemo.java
@@ -0,0 +1,28 @@
+package pers.xuhan.java.javabasic.并发编程.读写锁;
+
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+class ReadWriterLockDemo{
+    private int number = 0;
+    private ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+    //读(可以多个线程同时操作)
+    public void get(){
+        readWriteLock.readLock().lock();//上锁
+        try {
+            System.out.println(Thread.currentThread().getName()+":"+number);
+        }finally {
+            readWriteLock.readLock().unlock();//释放锁
+        }
+    }
+    //写(一次只能有一个线程操作)
+    public void set(int number){
+        readWriteLock.writeLock().lock();
+        try {
+            System.out.println(Thread.currentThread().getName());
+            this.number = number;
+        }finally {
+            readWriteLock.writeLock().unlock();
+        }
+    }
+}
